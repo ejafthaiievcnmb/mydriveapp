42480717ffa66b73ff7873fb9cc2b2f712c11f3d
Emmanuel Jaftha <ejaftha@iie.ac.za>
2025-09-03 14:54:08 +0200

    we have added licenseyears.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const age = 15;
+  const licenseyears = 5;
+  let feedback = '';
+
+  if (age >= 18) 
+            if (licenseyears >= 3) 
+            {
+                feedback = 'You are a qualified, safe driver.';
+            } else
+  {
+      feedback = 'You are allowed to drive. YAY!'
+    } else 
+                {
+                  feedback = 'You are not allowed drive yet.'
+                }
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text style={styles.response}>
+      {feedback} {/* This is my message to be displayed to the user */ }
+      </Text>
+     
     </View>
   );
 }
@@ -13,8 +32,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8d24aaff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+    response: {
+      fontSize: 28,
+      color: 'white',
+      fontWeight: 'bold'
+    }
+
 });
